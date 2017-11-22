02099bef487b6484878e7bd5fdba3481ae69ec5f
sarang <sarang05me43@gmail.com>
2017-11-22 13:56:04 -0600

    Modified template. Text position will be input manually. Create directories for each workshop

--- Certificate.py
+++ Certificate.py
@@ -1,48 +1,73 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-
+from datetime import datetime
+import os
 
 class Certicicate:
 	def __init__(self, title, name, date):
-		self.template = '0.jpg'
+		self.template = 'certificate_template/0.jpg'
 		self.title = title
 		self.name = name
 		self.date = date
 
 	def generate(self):
-		footer = "attended the workshop on {} that was held on {}".format(self.title, self.date)
 		img = Image.open(self.template,'r')
-		img_width, img_height = img.size
-		font_type = ImageFont.truetype("arial.ttf", 25)
-		font_type_footer  = ImageFont.truetype("arial.ttf", 15)
 		draw = ImageDraw.Draw(img)
-		user_name = self.name
-		text_size = draw.textsize(user_name, font_type)
-		footer_size = draw.textsize(footer, font_type_footer)
+
+		name_font_object = ImageFont.truetype("arial.ttf", 64)
+		footer_font_object  = ImageFont.truetype("arial.ttf", 35)
+		
+		name = self.name.upper()
+		footer = "{} that was held on {}".format(self.title, self.date)
+
+		# Settings from sketch
+		scale_factor = 1.5
+		name_x = 101*scale_factor
+		name_y = 467*scale_factor
+
+		footer_x = 101*scale_factor
+		footer_y = 640*scale_factor
+
+		"""
 		text_width = text_size[0]
 		text_height = text_size[1]
 		footer_width = footer_size[0]
 		footer_height = footer_size[1]
+
 
 		text_x = (img_width-text_width)*0.5
 		text_y = (img_height-text_height)*0.5
 
 		footer_x = (img_width-footer_width)*0.5
 		footer_y = (img_height-footer_height)*0.6
+		"""
 
-		draw.text((text_x, text_y),user_name, font=font_type)
-		draw.text((footer_x, footer_y),footer, font=font_type_footer)
+		draw.text((name_x, name_y),name, font=name_font_object, fill=(0,0,0,0))
+		draw.text((footer_x, footer_y),footer, font=footer_font_object, fill=(0,0,0,0))
+		t = datetime.today()
+		date = t.strftime('%Y-%m-%d')
+		fdir = self.title + '_' + date
+		fdir = os.path.join(os.getcwd(), fdir)
+		if not os.path.isdir(fdir): os.mkdir(fdir)
+
 		fname = self.name + '.jpg'
 		fname = fname.lower().replace(' ','_')
-		print(fname)
+		fname = os.path.join(fdir, fname)
+		
 		img.save(fname, subsampling=0, quality=100, mode='RGB')
 
-workshop_name = 'Advanced CFD workshop'
-workshop_date = 'October 15, 2017'
+
+workshop_name = 'Advanced Crash worthiness'
+workshop_date = 'November 19, 2017'
 
 for user in open('users.txt'):
 	u =user.strip().replace('.','')
+	if len(u)>0:
+		c1 = Certicicate(workshop_name, u, workshop_date)
+		c1.generate()
 
-	c1 = Certicicate(workshop_name, u.upper(), workshop_date)
-	c1.generate()
+
+
+
+
